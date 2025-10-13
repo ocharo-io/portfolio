92ec8744ba32354944dd956029e7a002c5fedefd
stealth <lilocharo34@gmail.com>
2025-10-14 00:28:35 +0300

    g

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,22 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
+import Nav from "./Nav";
 const Header = () => {
-    return <header>Header</header>;
+    return (
+         <header className="py-8 xl:py-12 text-white">
+           <div className="container mx-auto">
+            {/*logo */}
+            <Link href="/">
+              <h1 className="text-4xl font-semibold">
+                Cyprian<span> className="text-accent".</span>
+              </h1>
+            </Link>
+            {/* desktop nav */}
+            <Nav />
+           </div>
+        </header>
+    );
 }; 
 
 export default Header
