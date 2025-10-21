5c9bf2e8e63562e5c060f268897d839833e29925
stealth <lilocharo34@gmail.com>
2025-10-21 05:03:48 +0300

    new ish

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,23 +5,53 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
+    <div className="relative w-full h-full flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 1.2, duration: 0.8, ease: "easeInOut" },
-        }}
-        className="w-[298px] h-[298px] xl:w-[494px] xl:h-[498px] mix-blend-lighten relative"
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1.2, duration: 0.8, ease: "easeInOut" }}
+        className="relative w-[298px] h-[298px] xl:w-[494px] xl:h-[498px] mix-blend-lighten"
       >
         <Image
           src="/assets/_.jpeg"
-          alt=""
+          alt="Profile image"
           fill
           priority
           quality={100}
-          className="object-contain"
+          className="object-contain select-none pointer-events-none"
         />
+        <motion.svg
+          width="100%"
+          height="100%"
+          viewBox="0 0 506 506"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute top-0 left-0"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0", rotate: 0 }}
+            animate={{
+              strokeDasharray: [
+                "15 120 25 25",
+                "16 25 92 72",
+                "4 250 22 22",
+              ],
+              rotate: [0, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: "linear",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
